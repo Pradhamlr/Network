1795c96a58eeaddc5bdf5673413f87b0b9bb3219
Pradhamlr <147630347+Pradhamlr@users.noreply.github.com>
2026-03-09 12:29:22 +0530

    Update distance.c

--- ns2/distance.c
+++ ns2/distance.c
@@ -1,64 +1,69 @@
-#include <stdio.h>
-#define INFINITY 9999
-#define MAX 10
-
-int cost[MAX][MAX], dist[MAX][MAX], next_hop[MAX][MAX];
-int nodes;
-
-void initialize() {
-    for (int i = 0; i < nodes; i++) {
-        for (int j = 0; j < nodes; j++) {
-            dist[i][j] = cost[i][j];
-            next_hop[i][j] = j;
-        }
-    }
+dv.tcl :
+set ns [new Simulator]
+$ns rtproto DV
+set nf [open dv1.tr w]
+$ns trace-all $nf
+set nr [open dv2.nam w]
+$ns namtrace-all $nr
+proc finish {} {
+ global ns nf nr
+ $ns flush-trace
+ close $nf
+ close $nr
+ exit 0
+ #exec nam dv2.nam
 }
-
-void updateRoutes() {
-    int updated;
-    do {
-        updated = 0;
-        for (int i = 0; i < nodes; i++) {
-            for (int j = 0; j < nodes; j++) {
-                for (int k = 0; k < nodes; k++) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
-                        next_hop[i][j] = next_hop[i][k];
-                        updated = 1;
-                    }
-                }
-            }
-        }
-    } while (updated);
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n0 1Mb 10ms DropTail
+set udp0 [new Agent/UDP]
+$ns attach-agent $n0 $udp0
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $udp0
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.005
+set null0 [new Agent/Null]
+$ns attach-agent $n3 $null0
+$ns connect $udp0 $null0
+$ns at .1 "$cbr0 start"
+$ns at 45.0 "$cbr0 stop"
+$ns at 50.0 "finish"
+$ns run
+$ns attach-agent $n0 $udp0
+AWK Program for DV
+BEGIN {
+ print "Performance evaluation"
+ send = 0
+ recv = 0
+ dropped = 0
+ rout = 0
 }
-
-void display() {
-    for (int i = 0; i < nodes; i++) {
-        printf("\nRouter %d's Routing Table:\n", i + 1);
-        printf("Destination\tCost\tNext Hop\n");
-        for (int j = 0; j < nodes; j++) {
-            printf("%d\t\t%d\t%d\n", j + 1, dist[i][j], next_hop[i][j] + 1);
-        }
-    }
+{
+ if ($1 == "+" && ($3 == "0" || $3 == "1") && $5 == "cbr") {
+ send++
+ }
+ if ($1 == "r" && $4 == "3" && $5 == "cbr") {
+ recv++
+ }
+ if ($1 == "d") {
+ dropped++
+ }
+ if ($1 == "r" && $5 == "rtProtoDV") {
+ rout++
+ }
 }
-
-int main() {
-    printf("Enter the number of routers: ");
-    scanf("%d", &nodes);
-
-    printf("Enter the cost matrix (enter 9999 for no direct link):\n");
-    for (int i = 0; i < nodes; i++) {
-        for (int j = 0; j < nodes; j++) {
-            scanf("%d", &cost[i][j]);
-            if (i == j) {
-                cost[i][j] = 0;
-            }
-        }
-    }
-
-    initialize();
-    updateRoutes();
-    display();
-
-    return 0;
+END {
+ print "No of packets Send: " send
+ print "No of packets Received: " recv
+ print "No of packets dropped: " dropped
+ print "No of routing packets: " rout
+ NOH = rout / recv
+ PDR = recv / send
+ print "Normalised overhead: " NOH
+ print "Packet delivery ratio: " PDR
 }
